ab4fe49015a170ff6473cd73711f192d25320375
Catalina Kalman <kalmanmariacatalina@gmail.com>
2023-08-28 18:16:40 +0100

    Basics - strings

---
+++ string-methods.py
@@ -0,0 +1,10 @@
+text = "My name is my_name"
+
+print(f"Lenght of string: {len(text)}")
+
+text = text.replace("my_name", "Kay")
+print(f"Replace text: {text}")
+
+print(f"All uppercase: {text.upper()}")
+
+print(f"All lowercase: {text.lower()}")
