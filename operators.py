207994d4d70f2e9ad6fc9dc6490e6b59146ef78a
Catalina Kalman <kalmanmariacatalina@gmail.com>
2023-08-28 17:35:52 +0100

    Basics - operators

---
+++ operators.py
@@ -0,0 +1,30 @@
+# == Subtraction ==
+
+subtracted = 2 - 3
+print(f"2 ? 3 = {subtracted} (should be -1)")
+
+# == Division ==
+
+divided = 2 / 3
+print(f"2 ? 3 = {divided} (should be 0.6666666666666666)")
+
+# This kind of 'decimal point' number, 0.6666666666666666 is
+# called a float, by the way, meaning 'floating point'.
+
+# == Modulus ==
+# Sometimes known as "remainder if we divide 3 by 2"
+
+modulus = 3 % 2
+print(f"3 ? 2 = {modulus} (should be 1)")
+
+# == Floor division ==
+# Sometimes known as "division without remainder"
+
+floor_divided = 2 // 3
+print(f"2 ? 3 = {floor_divided} (should be 0)")
+
+# == Exponentiation ==
+# Sometimes known as "2 to the power of 3"
+
+expr = 2 ** 3
+print(f"2 ? 3 = {expr} (should be 8)")
