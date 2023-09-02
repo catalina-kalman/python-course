8a7633f3f4782a213ff3e2abc86e56b462e06309
Catalina Kalman <kalmanmariacatalina@gmail.com>
2023-09-02 22:33:12 +0100

    Basics - final exercises

--- Basics/challenge2_solution.py
+++ Basics/challenge2_solution.py
@@ -1,5 +1,5 @@
 # Below is the same program as in the example. Your
-# challenge is to implement some improvements:
+# challenge is to implement some improvements (original code is at the bottom):
 
 # 1. Right now users can place their tiles over the other
 #    user's tiles. Prevent this.
@@ -142,6 +142,100 @@ def is_game_over(board):
   return True # If we get here, it is a draw
 
 # And test it out:
+print("Game time!")
+play_game()
+
+
+
+
+
+'''
+# Original code:
+
+def play_game():
+  board = [
+    [".", ".", "."],
+    [".", ".", "."],
+    [".", ".", "."]
+  ]
+  player = "X"
+  while not is_game_over(board):
+    print(print_board(board))
+    print("It's " + player + "'s turn.")
+    # `input` asks the user to type in a string
+    # We then need to convert it to a number using `int`
+    row = int(input("Enter a row: "))
+    column = int(input("Enter a column: "))
+    board = make_move(board, row, column, player)
+    if player == "X":
+      player = "O"
+    else:
+      player = "X"
+  print(print_board(board))
+  print("Game over!")
+
+def print_board(board):
+  formatted_rows = []
+  for row in board:
+    formatted_rows.append(" ".join(row))
+  grid = "\n".join(formatted_rows)
+  return grid
+
+def make_move(board, row, column, player):
+  board[row][column] = player
+  return board
+
+
+# This function will extract three cells from the board
+def get_cells(board, coord_1, coord_2, coord_3):
+  return [
+    board[coord_1[0]][coord_1[1]],
+    board[coord_2[0]][coord_2[1]],
+    board[coord_3[0]][coord_3[1]]
+  ]
+
+# This function will check if the group is fully placed
+# with player marks, no empty spaces.
+def is_group_complete(board, coord_1, coord_2, coord_3):
+  cells = get_cells(board, coord_1, coord_2, coord_3)
+  return "." not in cells
+
+# This function will check if the group is all the same
+# player mark: X X X or O O O
+def are_all_cells_the_same(board, coord_1, coord_2, coord_3):
+  cells = get_cells(board, coord_1, coord_2, coord_3)
+  return cells[0] == cells[1] and cells[1] == cells[2]
+
+# We'll make a list of groups to check:
+
+groups_to_check = [
+  # Rows
+  [(0, 0), (0, 1), (0, 2)],
+  [(1, 0), (1, 1), (1, 2)],
+  [(2, 0), (2, 1), (2, 2)],
+  # Columns
+  [(0, 0), (1, 0), (2, 0)],
+  [(0, 1), (1, 1), (2, 1)],
+  [(0, 2), (1, 2), (2, 2)],
+  # Diagonals
+  [(0, 0), (1, 1), (2, 2)],
+  [(0, 2), (1, 1), (2, 0)]
+]
+
+def is_game_over(board):
+  # We go through our groups
+  for group in groups_to_check:
+    # If any of them are empty, they're clearly not a
+    # winning row, so we skip them.
+    if is_group_complete(board, group[0], group[1], group[2]):
+      if are_all_cells_the_same(board, group[0], group[1], group[2]):
+        return True # We found a winning row!
+        # Note that return also stops the function
+  return False # If we get here, we didn't find a winning row
+
+# And test it out:
 
 print("Game time!")
 play_game()
+
+'''
